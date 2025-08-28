0d6f47ce078dbac11df9f2bd92094b5a341b87f6
4z4c <arturoyz2105@gmail.com>
2025-08-28 17:20:58 -0500

    C1TSAPP

---
+++ src/router/AppRoute.tsx
@@ -0,0 +1,79 @@
+import { createBrowserRouter, Navigate } from "react-router";
+
+import { ShopLayout } from "../shop/layouts/ShopLayout";
+import { HomePage } from "../shop/pages/home/HomePage";
+import { ProductPage } from "../shop/pages/product/ProductPage";
+import { GenderPage } from "../shop/pages/gender/GenderPage";
+
+import { LoginPage } from "../auth/page/login/LoginPage";
+import { RegisterPage } from "../auth/page/register/RegisterPage";
+
+import { AdminDasboardPage } from "../admin/pages/dashboard/AdminDasboardPage";
+import { AdminProductsPage } from "../admin/pages/products/AdminProductsPage";
+import { AdminProductPage } from "../admin/pages/product/AdminProductPage";
+import { lazy } from "react";
+
+const AuthLayout = lazy(() => import("../auth/layouts/AuthLayout"));
+const AdminLayout = lazy(() => import('../admin/layouts/AdminLayout'));
+
+
+export const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <ShopLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'product/:idSlug',
+        element: <ProductPage />
+      },
+      {
+        path: 'gender/:gender',
+        element: <GenderPage />
+      }
+    ]
+  },
+  {
+    path: '/auth',
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to='/auth/login' />
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />
+      }
+    ]
+  },
+  {
+    path: '/admin',
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminDasboardPage />
+      },
+      {
+        path: 'products',
+        element: <AdminProductsPage />
+      },
+      {
+        path: 'product/:id',
+        element: <AdminProductPage />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <Navigate to='/' />
+  }
+])
