bc62617a1b799cb9e5134572867d0b6ecb4cf84d
4z4c <arturoyz2105@gmail.com>
2025-08-31 23:51:18 -0500

    C7TSAPP

--- src/shop/actions/getProducts.action.ts
+++ src/shop/actions/getProducts.action.ts
@@ -4,17 +4,27 @@ import type { ProductsResponse } from "@/types/response/products.response";
 interface Options {
   limit?: number | string;
   offset?: number | string;
+  gender?: string;
+  sizes?: string;
+  minPrice?: number;
+  maxPrice?: number;
+  query?: string;
 }
 
 export const getProductsAction = async (
   options: Options
 ): Promise<ProductsResponse> => {
-  const { limit, offset } = options;
+  const { limit, offset, gender, sizes, minPrice, maxPrice, query } = options;
 
   const { data } = await tesloApi.get<ProductsResponse>("/products", {
     params: {
       limit,
       offset,
+      gender,
+      sizes,
+      minPrice,
+      maxPrice,
+      q: query,
     },
   });
 
